67db648cafeca29bba398c81ac2d10ab30d95c75
Upasana Singh <upasana_singh@epam.com>
2026-09-23 19:46:58 +0530

    update main.py

--- work/module03-task/main.py
+++ work/module03-task/main.py
@@ -1,6 +1,6 @@
 """Run a simple calculator example."""
 
-from calculator import add, subtract
+from calculator import add, multiply, subtract
 
 
 first_number = 10
@@ -8,5 +8,6 @@ second_number = 4
 
 print(f"{first_number} + {second_number} = {add(first_number, second_number)}")
 print(f"{first_number} - {second_number} = {subtract(first_number, second_number)}")
+print(f"{first_number} * {second_number} = {multiply(first_number, second_number)}")
 
 "testing git change"
