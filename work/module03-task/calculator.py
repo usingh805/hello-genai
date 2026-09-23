1ad6bd214f3845563948ebfec102bcc9cb5b29fc
Upasana Singh <upasana_singh@epam.com>
2026-09-23 19:45:01 +0530

    added multiply

--- work/module03-task/calculator.py
+++ work/module03-task/calculator.py
@@ -9,3 +9,8 @@ def add(first_number, second_number):
 def subtract(first_number, second_number):
     """Return the difference between two numbers."""
     return first_number - second_number
+
+
+def multiply(first_number, second_number):
+    """Return the product of two numbers."""
+    return first_number * second_number
